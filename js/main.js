d56d112085b34eb7de740901c9c398dc7a461e65
Gabriel Santos <gabriad@hotmail.com>
2021-08-14 19:53:04 -0300

    Incluindo JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+alert("bem vindo");
+function clique(elemento){
+document.getElementById("trocar").innerHTML = "Obrigado por confirmar sua idade.";
+    var idade = prompt("Digite sua idade.");
+if (idade>=18){
+    alert("Maior de idade, pode continuar utilizando nosso site.")
+;} else{alert("Favor esperar até ter idade para acessar meu conteúdo.");}   
+}
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por vir confirmar";
+}
